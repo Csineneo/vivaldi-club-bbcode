643a0f16474019767f63bfc7e1cd362a9b43ed5b
Csineneo <csineneo@gmail.com>
2019-01-09 11:03:51 +0800

    add font-awesome bbcode

--- extend.php
+++ extend.php
@@ -14,4 +14,19 @@ return (new Extend\Formatter)
 			'<video width="100%" controls><source src="{URL}" type="video/mp4">你的瀏覽器不支持 HTML5 視頻，請使用 Vivaldi 瀏覽器訪問。</video>'
 		);
 
+		$config->BBCodes->addCustom(
+			'[fa]{TEXT}[/fa]',
+			'<i class="icon fa fa-fw fa-{TEXT}"></i>'
+		);
+
+		$config->BBCodes->addCustom(
+			'[fab]{TEXT}[/fab]',
+			'<i class="fab fa-{TEXT}"></i>'
+		);
+
+		$config->BBCodes->addCustom(
+			'[fas]{TEXT}[/fas]',
+			'<i class="fas fa-{TEXT}"></i>'
+		);
+
 	});
